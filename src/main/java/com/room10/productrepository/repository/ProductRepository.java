d42290af4b812b3b204eced66d966ebb6c6cc3ab
farhanqinoyz <farhanqinoyz@gmail.com>
2023-05-27 18:50:55 +0700

    [Farhan] Product Interface Update

--- src/main/java/com/room10/productrepository/repository/ProductRepository.java
+++ src/main/java/com/room10/productrepository/repository/ProductRepository.java
@@ -1,7 +1,8 @@
+package com.room10.productrepository.repository;
+
+import com.room10.productrepository.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.productservice.model.Product;
 
 @Repository("ProductRepository")
 public interface ProductRepository extends JpaRepository<Product, String > {
